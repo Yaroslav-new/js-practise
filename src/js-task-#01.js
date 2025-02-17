410618e4dbd6091ea53e9d42d0d947775bde856f
Yaroslav <yaroslav.berezhnyi.nl@gmail.com>
2025-02-17 17:51:16 +0100

    Updated solved task #01

--- src/js-task-#01.js
+++ src/js-task-#01.js
@@ -1,18 +1,12 @@
-function getRandomNumbers () {
-    return Math.floor(Math.random() * 201) - 100;
-};
-const arrayA = Array.from({ length: 50 }, getRandomNumbers)
+const arrayA = [10, 5, 3, 0, 1, 2];
 
 function solution(arrayA) {
-    
     const numberSet = new Set(arrayA.filter(num => num > 0));
-
     let smallerNumber = 1;
-    
     while (numberSet.has(smallerNumber)) {
         smallerNumber++;
-    };
+    }
     return smallerNumber;
-};
+}
 
-console.log(solution(arrayA));
+console.log(solution(arrayA)); 
