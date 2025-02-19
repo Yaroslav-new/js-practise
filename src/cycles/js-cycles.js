69f5ab68d6f4cb2ec3f95d084fd743443ff6e4e1
Yaroslav <yaroslav.berezhnyi.nl@gmail.com>
2025-02-19 21:44:32 +0100

    added array for second solved task and console log

--- src/cycles/js-cycles.js
+++ src/cycles/js-cycles.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/59fb783bab11f89202001083
+// https://www.codewars.com/kata/5b6db1acb118141f6b000060 
 
 const items = [
     { type: 'newspaper', material: 'paper' },
@@ -27,46 +27,31 @@ function recycle(array) {
 
 console.log(recycle(items));
 
-// https://www.codewars.com/kata/5b6db1acb118141f6b000060
+// https://www.codewars.com/kata/59fb783bab11f89202001083
 
-function recycle(array) {
-  const container = {
-    paper: [],
-    glass: [],
-    organic: [],
-    plastic: []
-  };
+const recycle = [1, -1, 0, 2, -3, 0, 4, 5, -2];
 
-  for (let item of array) {
-    const { type, material, secondMaterial } = item;
-    if (container[material]) container[material].push(type);
-    if (secondMaterial && container[secondMaterial]) container[secondMaterial].push(type);
+function recycleMe(recycle){ 
+  let plasticCount = 0;
+  let glassCount = 0;
+  let cardCount = 0;
+
+  for (let item of recycle) {
+      if (item > 0) {
+          plasticCount++;
+      } else if (item < 0) {
+          glassCount++;
+      } else {
+          cardCount++;
+      }
   }
 
-  return [container.paper, container.glass, container.organic, container.plastic];
+  return [plasticCount, glassCount, cardCount];
 };
 
-// need to add console log and array
-
+console.log(recycleMe(recycle));
 
 // https://www.codewars.com/kata/57ecf6efc7fe13eb070000e1
-
-function outed(meet, boss){
-  let totaleppines = 0;
-  for (let key in meet) {
-    totaleppines +=meet[key];
-  };
-  
-  if (boss in meet) {
-    totaleppines +=meet[boss];
-  };
-  
-  const numberPeopeInRoom = Object.keys(meet).length;
-  
-  const avarageHappinesInRoom = totaleppines / numberPeopeInRoom;
-  
-  return avarageHappinesInRoom > 5 ? 'Nice Work Champ!' : 'Get Out Now!';
-};
 
 const meet = {
   'Alice': 6,
@@ -74,6 +59,22 @@ const meet = {
   'Charlie': 5,
   'Dave': 3,
   'boss': 7
+};
+
+function outed(meet, boss){
+  let totaleppines = 0;
+  for (let key in meet) {
+    totaleppines +=meet[key];
+      if (key === boss) {
+        totaleppines +=meet[boss];
+      }
+  }
+  
+  const numberPeopeInRoom = Object.keys(meet).length;
+  
+  const avarageHappinesInRoom = totaleppines / numberPeopeInRoom;
+  
+  return avarageHappinesInRoom > 5 ? 'Nice Work Champ!' : 'Get Out Now!';
 };
 
 console.log(meetingOutcome(meet, 'boss')); 
