267ac33fca9109b381c120afda0a00109735361e
Yaroslav <yaroslav.berezhnyi.nl@gmail.com>
2025-02-22 11:31:52 +0100

    small updates into files

--- src/interview-tasks/smallest_number.js
+++ src/interview-tasks/smallest_number.js
@@ -1,16 +1,19 @@
+/*
 // finding the smallest positive intenger (greater than 0) hat doesn't occur in array
 
 // funtion of random number from -100 to 100
 function getRandomNumbers () {
     return Math.floor(Math.random() * 201) - 100;
 };
+
 // defind length of array 
-const arrayRandomNumbers = Array.from({ length: 50 }, getRandomNumbers)
+const arrayRandomNumbers = Array.from({ length: 100 }, getRandomNumbers)
+*/
 
-// const arrayA = [10, 5, 3, 0, 1, 2];
+const arrayA = [10, 5, 3, 0, 1, 2];
 
-function solution(arrayRandomNumbers) {
-    const numberSet = new Set(arrayRandomNumbers.filter(num => num > 0));
+function solution(arrayA) {
+    const numberSet = new Set(arrayA.filter(num => num > 0));
     let smallerNumber = 1;
     while (numberSet.has(smallerNumber)) {
         smallerNumber++;
@@ -18,4 +21,4 @@ function solution(arrayRandomNumbers) {
     return smallerNumber;
 }
 
-console.log(solution(arrayRandomNumbers)); 
+console.log(solution(arrayA)); 
