4ec757c655a58e9a9e49cc6e63c0c5c3911600b7
Yaroslav <yaroslav.berezhnyi.nl@gmail.com>
2025-02-23 11:42:26 +0100

    Solved one task about ideal Square

--- src/js_traning_file.js
+++ src/js_traning_file.js
@@ -1,7 +1,7 @@
 import { getRandomNumbers, arrayRandomNumbers } from './utils.js';
 
 // Logic of finding an ideal Square
-const isSquare = function(arrayRandomNumbers){
+const isSquare = function (arrayRandomNumbers) {
     return arrayRandomNumbers.filter(randomNumber => randomNumber > 0 && Number.isInteger(Math.sqrt(randomNumber)))
 };
 
@@ -18,8 +18,34 @@ console.log(cockroachSpeed(1.08))
 // an array of words that transfer into a sentence
 const words = ['hello', 'world', 'this', 'is', 'great']
 
-function smash (words) {
+function smash(words) {
     return words.join(' ');
- };
+};
 
- console.log(smash(words));
+console.log(smash(words));
+
+/*
+Task:
+   Given an integral number, determine if it's a square number:
+
+   In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+*/
+const isSquareTask2 = function (number) {
+    for (let i = 0; i * i <= number; i++) {
+        if (i * i === number) {
+            return true;
+        }
+    }
+    return false;
+};
+
+console.log(isSquareTask2(10))
+
+// example 2 
+const isSquareTask3 = function (number) {
+    if (n < 0) return false; // Negative numbers cannot be perfect squares
+    let sqrt = Math.sqrt(number);
+    return Number.isInteger(sqrt);
+};
+
+
