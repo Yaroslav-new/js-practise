54f87d64c2da0199005edb717b8dd63ed2d4edae
Yaroslav <yaroslav.berezhnyi.nl@gmail.com>
2025-03-02 20:23:27 +0100

    Reduce solution task

--- src/interview-tasks/smallest_number.js
+++ src/interview-tasks/smallest_number.js
@@ -1,7 +1,6 @@
 const arrayA = [10, 5, 3, 0, 1, 2];
 
 // Foreach solution
-
 function solution(arrayA) {
     const numberSet = new Set();
     arrayA.forEach(num => { if (num > 0) numberSet.add(num); });
@@ -17,8 +16,22 @@ console.log(solution(arrayA));
 
 // Reduce solution 
 
+const arrayB = [10, 5, 3, 0, 1, 2];
 
+function solution(arrayB) {
+    const numberSet = arrayB.reduce((set, num) => {
+        if (num > 0) set.add(num);
+        return set;
+    }, new Set());
 
+    let smallerNumber = 1;
+    while (numberSet.has(smallerNumber)) {
+        smallerNumber++;
+    }
+    return smallerNumber;
+};
+
+console.log(solution(arrayB));
 
 // Task from js course 
 const task14Object = {
