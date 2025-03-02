13072a7b588ce4dfc04abb82d9d7da254b56da74
Yaroslav <yaroslav.berezhnyi.nl@gmail.com>
2025-03-02 19:53:11 +0100

    Added hardcode task from codewards about function

--- src/interview-tasks/smallest_number.js
+++ src/interview-tasks/smallest_number.js
@@ -16,3 +16,23 @@ function solution(arrayA) {
 console.log(solution(arrayA));
 
 // Reduce solution 
+
+
+
+
+// Task from js course 
+const task14Object = {
+	name: 'John',
+	lastName: 'Doe',
+	age: 25,
+	friend: 'wolf',
+	keys() {
+		for (let keys in this) {
+		}
+	},
+	call() {
+		return `My name is ${this.name} ${this.lastName}, and I am ${this.age} years old. My best friend is a ${this.friend}`
+	}
+};
+
+console.log(task14Object.call()); 
