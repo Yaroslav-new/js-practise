0cd39f7b506e2047830779ee4573a6edc49bab52
Yaroslav <yaroslav.berezhnyi.nl@gmail.com>
2025-03-02 20:33:20 +0100

    Removed one solved task from file smallest number

--- src/interview-tasks/smallest_number.js
+++ src/interview-tasks/smallest_number.js
@@ -32,20 +32,3 @@ function solution(arrayB) {
 };
 
 console.log(solution(arrayB));
-
-// Task from js course 
-const task14Object = {
-	name: 'John',
-	lastName: 'Doe',
-	age: 25,
-	friend: 'wolf',
-	keys() {
-		for (let keys in this) {
-		}
-	},
-	call() {
-		return `My name is ${this.name} ${this.lastName}, and I am ${this.age} years old. My best friend is a ${this.friend}`
-	}
-};
-
-console.log(task14Object.call()); 
