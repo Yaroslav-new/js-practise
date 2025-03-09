4acb196b5bdca542ef4ab70729a997d248a5aaa7
Yaroslav <yaroslav.berezhnyi.nl@gmail.com>
2025-03-09 20:30:07 +0100

    add to two solved tasks from codewars

--- src/codewars/arrays/arrays.js
+++ src/codewars/arrays/arrays.js
@@ -6,7 +6,7 @@ a can contain numbers or strings. x can be either.
 Return true if the array contains the value, false if not.
 */
 
-function check (a, x) {
+function check(a, x) {
     for (let i = 0; i < a.length; i++) {
         if (a[i] == x) {
             return true;
@@ -15,6 +15,19 @@ function check (a, x) {
     return false;
 }
 
-function check1 (a, x) {
+function check1(a, x) {
     return a.includes(x);
 }
+
+// Function to convert a name into initials
+function abbrevName(name) {
+    return name
+        .split(' ')
+        .map(letter => letter[0].toUpperCase())
+        .join(".")
+}
+
+// return new array with specified length and filled in with specified value
+let task4Func1 = (length, value) => {
+    return new Array(length).fill(value);
+};
